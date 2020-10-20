4683d693550c9c7d3551e26a52d58138b6583bb7
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-20 13:37:10 -0700

    added seed.py and installed bcrypt

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from bcrypt import bcrypt
 
 db = SQLAlchemy()
 
---
+++ seed.py
@@ -0,0 +1,25 @@
+from app import app
+from models import db, User
+from bcrypt import bcrypt
+
+db.drop_all()
+db.create_all()
+
+u1 = User(
+    username="awesome123",
+    password=bcrypt.generate_password_hash("password"),
+    email="user@email.com",
+    first_name="Bob",
+    last_name="Guy"
+)
+
+u2 = User(
+    username="wow321",
+    password=bcrypt.generate_password_hash("cool$tuff"),
+    email="something@email.com",
+    first_name="Joy",
+    last_name="Totheworld"
+)
+
+db.session.add_all([u1, u2])
+db.session.commit()
